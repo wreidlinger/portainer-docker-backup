ec45d22aec01f0a0fed75ba70f6fd63ba2a1035f
Wolfgang Reidlinger <100240865+wreidlinger@users.noreply.github.com>
2024-01-22 16:59:34 +0100

    Update docker-backup.sh

--- docker-backup.sh
+++ docker-backup.sh
@@ -5,10 +5,14 @@ SOURCE_DOCKER_COMPOSE="/var/lib/docker/volumes/portainer_data/_data/compose"
 SOURCE_DOCKER_CONTAINERS="/var/lib/docker/containers"
 SOURCE_DOCKER_VOLUMES="/var/lib/docker/volumes"
 #SOURCE_DOCKER_images=""
+echo -e "\n Set source directories:"
+echo -e "\n Docker Compose Data: $SOURCE_DOCKER_COMPOSE"
+echo -e "\n Docker Containers Data: $SOURCE_DOCKER_CONTAINERS"
+echo -e "\n Docker Volumes Data: $SOURCE_DOCKER_VOLUMES"
 
 # define, main backup destination location
 BACKUPDIR="/home/docktainer/backup"
-echo -e "\n $BACKUPDIR"
+echo -e "\n\n Set backup directory: $BACKUPDIR"
 
 # define number of local backups to keep
 NUM_KEEP_BACKUP=10
@@ -20,8 +24,8 @@ TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 # define names for the backup archives
 BACKUP_FILENAME_DOCKER_COMPOSE="${TIMESTAMP}-docker-compose.tar.gz"
 #echo -e "\n $BACKUP_FILENAME_DOCKER_COMPOSE"
-#BACKUP_FILENAME_DOCKER_CONTAINERS="${TIMESTAMP}-docker-containers.tar.gz"
-#BACKUP_FILENAME_DOCKER_VOLUMES="${TIMESTAMP}-docker-volumes.tar.gz"
+BACKUP_FILENAME_DOCKER_CONTAINERS="${TIMESTAMP}-docker-containers.tar.gz"
+BACKUP_FILENAME_DOCKER_VOLUMES="${TIMESTAMP}-docker-volumes.tar.gz"
 
 # Zielserver-Informationen
 #remote_user="root"
@@ -31,7 +35,6 @@ BACKUP_FILENAME_DOCKER_COMPOSE="${TIMESTAMP}-docker-compose.tar.gz"
 
 # build full path for the backup archives
 BACKUP_FULLPATH_DOCKER_COMPOSE="${BACKUPDIR}/${BACKUP_FILENAME_DOCKER_COMPOSE}"
-#echo -e "\n $BACKUP_FULLPATH_DOCKER_COMPOSE"
 BACKUP_FULLPATH_DOCKER_CONTAINERS="${BACKUPDIR}/${BACKUP_FILENAME_DOCKER_CONTAINERS}"
 BACKUP_FULLPATH_DOCKER_VOLUMES="${BACKUPDIR}/${BACKUP_FILENAME_DOCKER_VOLUMES}"
 
@@ -42,22 +45,24 @@ if [ ! -d $BACKUPDIR ]; then
 fi
 
 # terminal message
-echo -e "\n $TIMESTAMP Stopping Docker Containers:\n"
+echo -e "\n Stopping Docker Containers:\n"
 # shut down all current running docker containers
 #docker stop $(docker ps -q)
 #docker stop $(docker ps --format '{{.Names}}')
 
 # terminal message
-echo -e "\n $TIMESTAMP Start Backup Docker Compose,Containers,Volumes Data.\n"
+echo -e "\n Start Backup Docker Compose,Containers,Volumes Data.\n"
 
 # create the comporessed backup archive files
 # c-Erstellen, p-Rechte behalten, f-Datei, z-Mit gzip komprimieren
 # --absolute-names
 # Don't strip leading slashes from file names when creating archives.
-echo -e "\n $TIMESTAMP Archiving and Compressing Docker Compose Data:\n"
-tar cfvz "${BACKUP_FULLPATH_DOCKER_COMPOSE}.tar.gz" "${SOURCE_DOCKER_COMPOSE}"
-#tar -cpfz "${BACKUP_FULLPATH_DOCKER_CONTAINERS}" --absolute-names "${SOURCE_DOCKER_CONTAINERS}"
-#tar -cpfz "${BACKUP_FULLPATH_DOCKER_VOLUMES}" --absolute-names "${SOURCE_DOCKER_VOLUMES}"
+echo -e "\n\n Archiving and Compressing Docker Compose Data:"
+tar cfvz "${BACKUP_FULLPATH_DOCKER_COMPOSE}" --absolute-names "${SOURCE_DOCKER_COMPOSE}"
+echo -e "\n\n Archiving and Compressing Docker Container Data:"
+tar cfvz "${BACKUP_FULLPATH_DOCKER_CONTAINERS}" --absolute-names "${SOURCE_DOCKER_CONTAINERS}"
+echo -e "\n\n Archiving and Compressing Docker Volumes Data:"
+tar cfvz "${BACKUP_FULLPATH_DOCKER_VOLUMES}" --absolute-names "${SOURCE_DOCKER_VOLUMES}"
 
 # restart ALL Docker-Container
 # missing improvement, restart only container not stopped bevor executing the line:
